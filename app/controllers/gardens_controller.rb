dafeb83c57cc4f0171a9b78e7ba3130bb6dd4136
alizee kraxner <a.kraxner@laposte.net>
2024-06-04 16:25:28 +0200

    fini

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,2 +1,17 @@
 class GardensController < ApplicationController
+  def new
+    @garden = Garden.new
+  end
+
+  def create
+    @garden = Garden.new(garden_params)
+    @garden.save
+    redirect_to gardens_path(@garden)
+  end
+
+  private
+
+  def garden_params
+    params.require(:garden).permit(:name, :size, :exposition, :address)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
 
   resources :garden_steps, only: [:show, :update]
 
-
   get  "gardens/:id", to: "gardens#packagechoice", as: "packagechoice"
+  get "gardens/new", to: "gardens#new"
+  post "gardens", to: "gardens#create"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
