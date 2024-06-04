75b05251b9f575cf89cc1cc07683ba30de253e35
lin xu <helado_a@hotmail.com>
2024-06-04 18:48:04 +0200

    gardensteps update

--- app/controllers/garden_steps_controller.rb
+++ app/controllers/garden_steps_controller.rb
@@ -1,8 +1,25 @@
 class GardenStepsController < ApplicationController
+  before_action :set_garden_step, only: [:show, :update]
+
   def show
-    @garden_step = GardenStep.find(params[:id])
     @step = @garden_step.step
     @tasks = @step.tasks
     @garden = @garden_step.garden
+  end
+
+  def update
+    set_garden_step
+    @garden_step.status.value.update(garden_step_params)
+    redirect_to garden_path(@garden)
+  end
+
+  private
+
+  def set_garden_step
+    @garden_step = GardenStep.find(params[:id])
+  end
+
+  def garden_step_params
+    params.require(:garden_step).permit(:status)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :garden_steps, only: [:create]
   end
 
-  resources :garden_steps, only: [:show, :update]
+  resources :garden_steps, only: [:show, :edit, :update]
 
   resources :gardens
   resources :steps, only: [:show]
