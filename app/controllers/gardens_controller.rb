212945ff981db8c6b7512f158ab975e54a1a51b4
alizee kraxner <a.kraxner@laposte.net>
2024-06-10 14:40:59 +0200

    début de la fin

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,9 +1,9 @@
 class GardensController < ApplicationController
-  def new
-    @garden = Garden.new
-  end
+  before_action :set_garden, only: [:edit, :next_step, :previous_step]
+# :update dans only si retour a la normal
 
   def create
+    raise
     @garden = Garden.new(garden_params)
     @garden.user = current_user
     @garden.save
@@ -26,13 +26,81 @@ class GardensController < ApplicationController
     redirect_to gardens_path, status: :see_other
   end
 
-
   def packagechoice
     @garden = Garden.find(params[:id])
     @packages = Package.where(size: @garden.size)
   end
 
+  def new
+    @garden = Garden.new
+    @garden.current_step = 1
+    render :step1
+  end
+
+  def edit
+    raise
+    case @garden.current_step
+    when 1
+      render :step1
+    when 2
+      raise
+      render :step2
+    when 3
+      render :step3
+    when 4
+      render :step4
+    else
+      redirect_to @garden
+    end
+  end
+
+  def next_step
+    @garden.assign_attributes(garden_params)
+    if params[:next]
+      @garden.current_step += 1 if @garden.valid?
+    elsif params[:back]
+      @garden.current_step -= 1 if @garden.current_step > 1
+    end
+
+    @garden.save(validate: false)
+
+    case @garden.current_step
+    when 1
+      render :step1
+    when 2
+      render :step2
+    when 3
+      render :step3
+    when 4
+      render :step4
+    else
+      redirect_to @garden, notice: 'Garden was successfully created.'
+    end
+  end
+
+  def previous_step
+    @garden.current_step -= 1 if @garden.current_step > 1
+    @garden.save(validate: false)
+
+    case @garden.current_step
+    when 1
+      render :step1
+    when 2
+      render :step2
+    when 3
+      render :step3
+    when 4
+      render :step4
+    else
+      redirect_to @garden, notice: 'Garden was successfully created.'
+    end
+  end
+
   private
+
+  def set_garden
+    @garden = Garden.find(params[:id])
+  end
 
   def garden_params
     params.require(:garden).permit(:name, :size, :exposition, :address)
